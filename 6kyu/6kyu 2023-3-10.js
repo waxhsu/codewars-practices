324bdf0a2549fe5a6a879aee2d5fb093aa15639a
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-03-10 11:21:01 -0500

    daily practice

---
+++ 6kyu/6kyu 2023-3-10.js
@@ -0,0 +1,42 @@
+// https://www.codewars.com/kata/5842df8ccbd22792a4000245/train/javascript
+
+function expandedForm(num) {
+    // Your code here
+  }
+
+// MY ANSWER
+
+
+// https://www.codewars.com/kata/54da539698b8a2ad76000228/train/javascript
+
+// MY ANSWER
+function isValidWalk(walk) {
+  let sum = 0
+  console.log(walk)
+  if (walk.length !== 10){
+    return false 
+  } else {
+    walk.forEach((item,index) => {
+      if (walk[index] == 'n'){
+        sum += 1
+      } else if (walk[index] == 's'){
+        sum -= 1
+      } else if (walk[index] == 'e'){
+        sum += 2
+      } else {
+        sum -= 2
+      }
+    })
+  if (sum == 0){
+    return true
+  } else {
+    return false
+  }
+  }
+}
+
+// SHORTER OLUTION
+
+// https://www.codewars.com/kata/54da539698b8a2ad76000228/solutions/javascript
+// there's multiple solutions but all interesting
+
