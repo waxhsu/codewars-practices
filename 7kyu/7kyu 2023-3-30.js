b7a4bb01ae8c78bcb8858c10988c6e737bb2a6ad
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-03-30 11:18:43 -0400

    Update 7kyu 2023-3-30.js

--- 7kyu/7kyu 2023-3-30.js
+++ 7kyu/7kyu 2023-3-30.js
@@ -19,3 +19,33 @@ function findNextSquare(sq) {
       }
   }
   
+
+  // https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/javascript
+
+
+// MY SOLUTION
+function greet(language) {
+    const choices = {
+      english: 'Welcome',
+      czech: 'Vitejte',
+      danish: 'Velkomst',
+      dutch: 'Welkom',
+      estonian: 'Tere tulemast',
+      finnish: 'Tervetuloa',
+      flemish: 'Welgekomen',
+      french: 'Bienvenue',
+      german: 'Willkommen',
+      irish: 'Failte',
+      italian: 'Benvenuto',
+      latvian: 'Gaidits',
+      lithuanian: 'Laukiamas',
+      polish: 'Witamy',
+      spanish: 'Bienvenido',
+      swedish: 'Valkommen',
+      welsh: 'Croeso',
+    }
+    
+return choices[language] || 'Welcome'
+    
+    }
+
